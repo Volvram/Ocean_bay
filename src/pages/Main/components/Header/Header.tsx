d573b28c21f846fcec88d434f44409ff9cbcfb57
Julia <yuliabobrovich@yandex.ru>
2022-09-25 19:44:12 +0300

    Added block about us

--- src/pages/Main/components/Header/Header.tsx
+++ src/pages/Main/components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import styles from "./styles.module.scss";
 
 const Header: React.FC = () => {
@@ -8,17 +10,34 @@ const Header: React.FC = () => {
       <div className={styles.header_menu}>
         <img alt="ocean bay" className={styles.header_menu_logo}></img>
         <div className={styles.header_menu_choice}>
-          <a href="" className={styles.header_menu_choice_a}>О НАС</a>
+          <Link
+            to={{ pathname: "/", hash: "aboutUs" }}
+            className={styles.header_menu_choice_link}
+          >
+            О НАС
+          </Link>
         </div>
         <div className={styles.header_menu_choice}>
-          <a href="" className={styles.header_menu_choice_a}>АССОРТИМЕНТ</a>
+          <Link
+            to={{ pathname: "/", hash: "assortment" }}
+            className={styles.header_menu_choice_link}
+          >
+            АССОРТИМЕНТ
+          </Link>
         </div>
         <div className={styles.header_menu_choice}>
-          <a href="" className={styles.header_menu_choice_a}>КОНТАКТЫ</a>
+          <Link
+            to={{ pathname: "/", hash: "contacts" }}
+            className={styles.header_menu_choice_link}
+          >
+            КОНТАКТЫ
+          </Link>
         </div>
       </div>
       <div className={styles.header_phone}>
-        <a href="tel: +79096353022" className={styles.header_phone_a}>+7(909)-635-30-22</a>
+        <a href="tel: +79096353022" className={styles.header_phone_a}>
+          +7(909)-635-30-22
+        </a>
       </div>
     </div>
   );
