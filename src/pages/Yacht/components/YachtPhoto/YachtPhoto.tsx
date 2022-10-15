949b952415abebfd1a0437608da1cae95cc82e8c
Julia <yuliabobrovich@yandex.ru>
2022-10-15 20:10:04 +0300

    Added carousel lib

--- src/pages/Yacht/components/YachtPhoto/YachtPhoto.tsx
+++ src/pages/Yacht/components/YachtPhoto/YachtPhoto.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Slider from "@components/Slider";
 import { YachtsData } from "@config/yachts";
+import Carousel from "nuka-carousel";
 
 import styles from "./styles.module.scss";
 
@@ -12,7 +12,13 @@ type YachtsPhotoProps = {
 const YachtPhoto: React.FC<YachtsPhotoProps> = ({ yacht }) => {
   return (
     <div className={styles.yachtPhoto}>
-      {yacht && <Slider items={yacht.additionalImages} />}
+      {yacht && (
+        <Carousel wrapAround={true} slidesToShow={2}>
+          {yacht.additionalImages.map((item) => {
+            return <img key={item.url} src={item.url} alt={item.title} />;
+          })}
+        </Carousel>
+      )}
     </div>
   );
 };
