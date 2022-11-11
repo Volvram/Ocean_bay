e6a30dacf963e27396e1fcfb3fc6ee586646bced
Julia <yuliabobrovich@yandex.ru>
2022-11-11 13:46:21 +0300

    Changed design of About us

---
+++ src/config/db/circularImages.ts
@@ -0,0 +1,27 @@
+import circle_1 from "@assets/img/circle_1.svg";
+import circle_10 from "@assets/img/circle_10.svg";
+import circle_11 from "@assets/img/circle_11.svg";
+import circle_12 from "@assets/img/circle_12.svg";
+import circle_2 from "@assets/img/circle_2.svg";
+import circle_3 from "@assets/img/circle_3.svg";
+import circle_4 from "@assets/img/circle_4.svg";
+import circle_5 from "@assets/img/circle_5.svg";
+import circle_6 from "@assets/img/circle_6.svg";
+import circle_7 from "@assets/img/circle_7.svg";
+import circle_8 from "@assets/img/circle_8.svg";
+import circle_9 from "@assets/img/circle_9.svg";
+
+export const circularImages: string[] = [
+  circle_1,
+  circle_2,
+  circle_3,
+  circle_4,
+  circle_5,
+  circle_6,
+  circle_7,
+  circle_8,
+  circle_9,
+  circle_10,
+  circle_11,
+  circle_12,
+];
--- src/pages/Main/components/AboutUs/AboutUs.tsx
+++ src/pages/Main/components/AboutUs/AboutUs.tsx
@@ -1,35 +1,103 @@
 import React from "react";
 
+import { circularImages } from "@config/db/circularImages";
+
 import "./AboutUs.scss";
 import styles from "./styles.module.scss";
 
 const AboutUs: React.FC = () => {
   return (
     <div id="aboutUs">
-      <div className={styles.aboutUs}>О НАС</div>
-      <div>
-        Яхт-клуб «OCEAN BAY» - уникальный порт, который предлагает своим
-        клиентам услуги высокого европейского качества.
-        <br />
-        <br />
-        Хотите совершить увлекательную прогулку, провести незабываемый
-        романтический вечер, оригинальное торжество, мечтаете о активном отдыхе,
-        а может ищете место для причала собственного судна? Яхт-клуб OCEAN BAY к
-        Вашим услугам!
-        <br />
-        <br />
-        Клуб предлагает увлекательные экскурсии, прогулки на моторных яхтах,
-        проведение мероприятий и фотосессий, а также возможность аренды яхты с
-        капитаном. Большой выбор яхт премиум класса, а также уютные каюты со
-        всеми удобствами. Если же вы имеете свою яхту и ищите место для её
-        причала, мы предлагаем наш порт для её размещения как на короткий, так и
-        на длительный промежуток времени.
-        <br />
-        <br />
-        Вы самостоятельно выбираете не только маршрут путешествия, но и время
-        отправления корабля! Ваша прогулка может стать уникальным путешествием с
-        остановками для купания и рыбалкой, активным отдыхом или путешествие по
-        живописным местам.
+      <div className={styles.aboutUs}>НАШ СЕРВИС</div>
+      <div className={styles.container}>
+        <div className={styles.container_services}>
+          <h1 className={styles.container_services_h}>СОБСТВЕННЫЙ ПОРТ.</h1>
+          <div className={styles.container_services_service}>
+            Большой выбор яхт премиум класса, уютные каюты со всеми удобствами.
+            Если же вы имеете свою яхту и ищите место для её причала, мы
+            предлагаем наш порт для её размещения как на короткий, так и на
+            длительный промежуток времени.
+          </div>
+
+          <h1 className={styles.container_services_h}>
+            ВЫБОР МАРШРУТА, ВРЕМЕНИ, ЗАНЯТИЙ.{" "}
+          </h1>
+          <div className={styles.container_services_service}>
+            Вы самостоятельно выбираете не только маршрут путешествия, но и
+            время отправления яхты! Ваша прогулка может стать уникальным
+            путешествием с остановками для купания и рыбалкой, активным отдыхом
+            или путешествие по живописным местам.
+          </div>
+
+          <h1 className={styles.container_services_h}>
+            ЭКСКУРСИИ, МЕРОПРИЯТИЯ, ФОТОСЕССИИ.
+          </h1>
+          <div className={styles.container_services_service}>
+            Хотите провести незабываемый романтический вечер, оригинальное
+            торжество, мечтаете об активном отдыхе? Ocean Bay предлагает
+            увлекательные экскурсии, проведение мероприятий и фотосессий, а
+            также аренды яхты с капитаном.
+          </div>
+        </div>
+
+        <div className={styles.container_images}>
+          <div className={styles.container_images_row}>
+            {circularImages.map((image, index) => {
+              if (index >= 0 && index < 3) {
+                return (
+                  <img
+                    key={`circle_${index + 1}`}
+                    src={image}
+                    className={styles.container_images_row_img}
+                    alt="circle"
+                  />
+                );
+              }
+            })}
+          </div>
+          <div className={styles.container_images_row}>
+            {circularImages.map((image, index) => {
+              if (index >= 3 && index < 6) {
+                return (
+                  <img
+                    key={`circle_${index + 1}`}
+                    src={image}
+                    className={styles.container_images_row_img}
+                    alt="circle"
+                  />
+                );
+              }
+            })}
+          </div>
+          <div className={styles.container_images_row}>
+            {circularImages.map((image, index) => {
+              if (index >= 6 && index < 9) {
+                return (
+                  <img
+                    key={`circle_${index + 1}`}
+                    src={image}
+                    className={styles.container_images_row_img}
+                    alt="circle"
+                  />
+                );
+              }
+            })}
+          </div>
+          <div className={styles.container_images_row}>
+            {circularImages.map((image, index) => {
+              if (index >= 9 && index < 12) {
+                return (
+                  <img
+                    key={`circle_${index + 1}`}
+                    src={image}
+                    className={styles.container_images_row_img}
+                    alt="circle"
+                  />
+                );
+              }
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
