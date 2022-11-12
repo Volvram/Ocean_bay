424bab43907e22fb7cdb165d3df16773cee08d28
Julia <yuliabobrovich@yandex.ru>
2022-11-12 14:10:05 +0300

    Created video background for reservation block and fixed some sizes

--- src/pages/Main/components/Reservation/Reservation.tsx
+++ src/pages/Main/components/Reservation/Reservation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import reservationBackground from "@assets/img/reservationBackground.svg";
+import waves from "@assets/video/waves.mp4";
 import Button from "@components/Button";
 import { Link } from "react-router-dom";
 
@@ -8,10 +9,17 @@ import styles from "./styles.module.scss";
 
 const Reservation: React.FC = () => {
   return (
-    <div
-      className={styles.reservationBackground}
-      style={{ backgroundImage: `url(${reservationBackground})` }}
-    >
+    <div className={styles.reservation}>
+      <video
+        className={styles.reservation_video}
+        src={waves}
+        preload="auto"
+        autoPlay
+        no-controls="true"
+        loop
+        playsInline
+        muted
+      />
       <div className={`${styles.reservation_h} element-animation`}>
         СВОБОДА. ЭМОЦИИ. OCEAN BAY.
       </div>
