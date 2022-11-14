a2ad97adca7e60fce523c1ea4026513f0b757084
Julia <yuliabobrovich@yandex.ru>
2022-11-14 19:05:40 +0300

    Video changed to hover animation

--- src/pages/Main/components/Assortment/components/YachtOffers/components/BestYacht/BestYacht.tsx
+++ src/pages/Main/components/Assortment/components/YachtOffers/components/BestYacht/BestYacht.tsx
@@ -23,7 +23,7 @@ const BestYacht: React.FC = () => {
             onMouseEnter={(e: React.MouseEvent) => setHover(true)}
             onMouseLeave={(e: React.MouseEvent) => setHover(false)}
           >
-            {hover ? (
+            {/* {hover ? (
               <video
                 className={styles.bestYachtOffer_offer_video}
                 src={bestYacht.video}
@@ -59,7 +59,29 @@ const BestYacht: React.FC = () => {
                   </div>
                 </div>
               </>
-            )}
+            )} */}
+            <img
+              src={bestYacht.image}
+              className={styles.bestYachtOffer_offer_img}
+              alt="yacht"
+            />
+            <div className={styles.bestYachtOffer_offer_price}>
+              {bestYacht.price}
+            </div>
+            <div className={styles.bestYachtOffer_offer_bestOffer}>
+              Лучшее предложение
+            </div>
+            <div className={styles.bestYachtOffer_offer_model}>
+              {bestYacht.model}
+            </div>
+            <div className={styles.bestYachtOffer_offer_made}>
+              <div className={styles.bestYachtOffer_offer_made_year}>
+                {bestYacht.year}
+              </div>
+              <div className={styles.bestYachtOffer_offer_made_place}>
+                {bestYacht.place}
+              </div>
+            </div>
           </div>
         </Link>
       )}
